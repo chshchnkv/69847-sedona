d0bba7a4aefbef8b52bdc7e138a6007381c1281a
Stan Chashchnikov <chashchnikov@gmail.com>
2015-10-20 01:12:55 +0300

    раскрывающееся меню

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+(function(){
+
+  var menuToggle = document.querySelector(".main-navigation__toggle");
+  var menuItems = document.querySelectorAll(".main-navigation__item");
+
+  console.log(menuItems);
+
+  menuToggle.addEventListener("click", function (event) {
+    event.preventDefault();
+    menuToggle.classList.toggle("main-navigation__toggle--close");
+
+    for (var i = 0; i < menuItems.length; i++) {
+      var menuItem = menuItems[i];
+
+      if (!menuItem.classList.contains("main-navigation__item--logo")) {
+        menuItem.classList.toggle("main-navigation__item--hidden");
+      }
+    }
+  })
+
+})();
