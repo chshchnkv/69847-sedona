46b52218bb9f8a263a12ae44af045ce5d878ff2c
Stan Chashchnikov <chashchnikov@gmail.com>
2015-10-20 14:55:27 +0300

    Валидация формы и редактирование количества путешественников

--- js/send.js
+++ js/send.js
@@ -5,19 +5,18 @@
   }
   
   var form = document.querySelector(".review-form");
-  
-  var allModals = document.querySelectorAll(".modal");  
-  for (var i=0; i<allModals.length; i++) {
-    var modal = allModals[i];
     
-    modal.querySelector(".button").addEventListener("click", function(event) {
-      event.preventDefault();
-      modal.classList.toggle("modal--show");
-    });
-  }
-  
   var errorModal = document.querySelector("#errorModal");
+    errorModal.querySelector(".button").addEventListener("click", function(event) {
+    event.preventDefault();
+    errorModal.classList.toggle("modal--show");
+  });
+
   var successModal = document.querySelector("#successModal");
+    successModal.querySelector(".button").addEventListener("click", function(event) {
+    event.preventDefault();
+    successModal.classList.toggle("modal--show");
+  });
   
   var queue = [];
   
@@ -32,6 +31,11 @@
   form.addEventListener("submit", function(event) {
     event.preventDefault();
     
+    if (!validateForm()) {
+      errorModal.classList.toggle("modal--show");
+      return;
+    }
+    
     var data = new FormData(form);
     
     queue.forEach(function(element) {
@@ -44,6 +48,11 @@
       successModal.classList.toggle("modal--show");
     });
   });
+  
+  function validateForm() {
+    var emptyRequiredElements = document.querySelectorAll("input:invalid");
+    return emptyRequiredElements.length == 0;
+  }
   
   function request(data, url, callback_func) {
     var xhr = new XMLHttpRequest();
@@ -97,4 +106,48 @@
     li.parentNode.removeChild(li);
   }
   
+  
+  // добавление/удаление путешественника
+  var travelersArea = document.querySelector(".travelers");
+  var travelerTemplate = document.querySelector("#travelers-item-template").innerHTML;
+  
+  var travelersCountInput = document.querySelector(".review-form-fields--travelers>.spinner>input");
+  
+  travelersCountInput.addEventListener("change", function(event) {
+    var currentTravelersCount = document.querySelectorAll(".travelers__item").length;
+    var needCount = travelersCountInput.value;
+    
+    if (isNaN(needCount)) {
+      count = 0;
+    }
+    
+    var change = needCount - currentTravelersCount;
+    if (change > 0) {
+      for (var i=1; i<=change; i++) {
+        addTraveler(currentTravelersCount + i)
+      }
+    } else if (change < 0) {
+      removeTravelers(needCount);
+    }
+    
+  });
+  
+  function addTraveler(id){
+    var html = Mustache.render(travelerTemplate, {"id": id});
+    
+    var li = document.createElement("li");
+    li.classList.add("travelers__item");
+    li.innerHTML = html;
+    
+    travelersArea.appendChild(li);
+  }
+  
+  function removeTravelers(startingId) {
+    var currentTravelersCount = document.querySelectorAll(".travelers__item").length;
+    
+    for (var i=currentTravelersCount; i>startingId; i--) {
+      var lastTraveler = travelersArea.querySelector(".travelers__item:last-child");
+      travelersArea.removeChild(lastTraveler);
+    }
+  }
 })();
